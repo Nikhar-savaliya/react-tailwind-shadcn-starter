63c7a065d9028f957fdb5a7778e6ba107e6dd0e6
Nikhar S. <127284370+Nikhar-savaliya@users.noreply.github.com>
2024-05-02 18:10:04 +0530

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <section className="mt-24">
+      <h1 className="h1">React + Tailwind + TypeScript + Shadcn/ui</h1>
+      <p className="h3">welcome, this is react starter code.</p>
+    </section>
+  );
+}
+
+export default App;
